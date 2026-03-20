34e1288354e23558a5225768a52a232a6f550a0d
veeran <sveeran877@gmail.com>
2026-03-20 13:59:36 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -3,38 +3,23 @@ window.onload = function () {
   const minuteHand = document.querySelector(".minuteHand");
   const secondHand = document.querySelector(".secondHand");
   const time = document.querySelector(".time");
-  const clock = document.querySelector(".clock");
-  const audio = document.querySelector(".audio");
 
   function setDate() {
     const today = new Date();
 
     const second = today.getSeconds();
-    const secondDeg = (second / 60) * 360 + 360;
+    const secondDeg = (second / 60) * 360;
     secondHand.style.transform = `rotate(${secondDeg}deg)`;
 
-    audio.play();
-
     const minute = today.getMinutes();
-    const minuteDeg = (minute / 60) * 360;
+    const minuteDeg = (minute / 60) * 360 + (second / 60) * 6; 
     minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
 
     const hour = today.getHours();
-    const hourDeg = (hour / 12) * 360;
+    const hourDeg = (hour / 12) * 360 + (minute / 60) * 30;
     hourHand.style.transform = `rotate(${hourDeg}deg)`;
 
-    time.innerHTML =
-      "<span>" +
-      "<strong>" +
-      hour +
-      "</strong>" +
-      " : " +
-      minute +
-      " : " +
-      "<small>" +
-      second +
-      "</small>" +
-      "</span>";
+    time.innerHTML = `<span><strong>${hour}</strong> : ${minute} : <small>${second}</small></span>`;
   }
 
   setInterval(setDate, 1000);
